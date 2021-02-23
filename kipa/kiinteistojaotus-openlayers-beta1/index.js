bb974716f492593b6d644756da1e1362817f4bae
nls-jajuko <nls-jajuko@users.noreply.github.com>
2021-02-23 18:38:34 +0200

    Update index.js

--- kipa/kiinteistojaotus-openlayers-beta1/index.js
+++ kipa/kiinteistojaotus-openlayers-beta1/index.js
@@ -1,15 +1,8 @@
-import 'ol/ol.css';
+ import 'ol/ol.css';
 import FullScreen from 'ol/control/FullScreen';
 import apply from 'ol-mapbox-style';
 import Map from 'ol/Map';
 
-const styleUrl = `https://beta-karttakuva.maanmittauslaitos.fi/kipa/WGS84_Pseudo-Mercator/kipa_kiinteistojaotus_simple_v1.json`;
-
-const map = new Map({ target: 'map' });
-
-fetch(styleUrl).then(r => r.json()).then(styleJson => {
-  apply(
-    map, styleJson).then(function (map) {
-      map.addControl(new FullScreen());
-    });
-});
+const styleUrl = `https://beta-karttakuva.maanmittauslaitos.fi/kipa/WGS84_Pseudo-Mercator/kipa_kiinteistojaotus_simple_v1.json`,
+  map = new Map({ target: 'map' });
+apply(map, styleUrl);
